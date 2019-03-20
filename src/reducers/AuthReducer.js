847c0ca475712d3f9b86b4c557b331f1a04df7d3
caricaDev <santos2tiago@gmail.com>
2019-03-19 23:31:59 -0300

    Implement  redux

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,35 @@
+import {EMAIL_CHANGED, 
+    PASSWORD_CHANGED, 
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGIN} from '../types'
+
+
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    user: null,
+    error: '',
+    loading: false
+}
+
+export default (state = INITIAL_STATE, action) =>{
+    console.log(action)
+    switch(action.type){
+        case EMAIL_CHANGED:
+            return {...state,email:action.payload}
+
+        case PASSWORD_CHANGED:
+            return { ...state, password: action.payload}
+
+        case USER_LOGIN:
+            return { ...state, loading: true}
+        case USER_LOGIN_SUCCESS:
+            return { ...state, ...INITIAL_STATE,user: action.payload}
+        case USER_LOGIN_FAIL:
+            return {...state, error: 'Authentication failed', loading: false}
+
+        default:
+            return state; 
+    }
+}
---
+++ src/types/index.js
@@ -0,0 +1,5 @@
+export const EMAIL_CHANGED = 'email_changed';
+export const PASSWORD_CHANGED = 'password_changed';
+export const USER_LOGIN_SUCCESS = 'login_success';
+export const USER_LOGIN_FAIL = 'login_fail';
+export const USER_LOGIN = 'login_user';
