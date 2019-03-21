ac7f458a4dc2312599165db811b9c0ae6e854256
caricaDev <santos2tiago@gmail.com>
2019-03-20 22:36:24 -0300

    install lodash e import listview

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
-import { View,Text } from 'react-native';
+import _ from 'lodash';
+import { View,Text, ListView } from 'react-native';
 import {connect} from 'react-redux';
 import {employeesFetch} from '../actions'
+import console = require('console');
 
 class EmployeeList extends Component {
-    componentWillMount(){
-        console.log(this.props.employeesFetch())
-
+    componentWillMount() {
+        this.props.employeesFetch();
+    
+        this.createDataSource(this.props);
     }
-
+    
+      componentWillReceiveProps(nextProps) {
+        // nextProps are the next set of props that this component
+        // will be rendered with
+        // this.props is still the old set of props
+    
+        this.createDataSource(nextProps);
+      }
+    
+      createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+          rowHasChanged: (r1, r2) => r1 !== r2
+        });
+    
+        this.dataSource = ds.cloneWithRows(employees);
+      }
     render(){
         return (
          <View>
@@ -25,5 +43,15 @@ class EmployeeList extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    console.log('antes',state.employees)
+    const employees = _.map(state.employees, (val, uid) => {
+      return { ...val, uid };
+    });
 
-export default connect(null, {employeesFetch})(EmployeeList);
+    console.log('depois',employees)
+  
+    return { employees };
+  };
+
+export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
